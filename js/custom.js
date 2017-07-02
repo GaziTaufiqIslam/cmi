a11bd95cf99976adf65ed38b7145aea093715d14
GaziTaufiqIslam <gzidev@gmail.com>
2017-07-03 01:56:15 +0530

    Serives section updated

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,20 @@
 $(document).ready(function(){
   $(".button-collapse").sideNav();
 
+if($(window).scrollTop>0)
+{
+  $('nav').removeClass("atop");
+}
+
+$(window).scroll(function (){
+  if($(this).scrollTop()>0)
+  {
+    $('nav').removeClass("atop");
+  }
+  else {
+    $('nav').addClass("atop");
+  }
+});
 //new code
 
 }); //document.ready ends here
